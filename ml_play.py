f821e19b618403d561551f943fde6b95aa73d658
abao1005 <52830937+abao1005@users.noreply.github.com>
2020-04-16 00:16:45 +0800

    Add files via upload

--- ml_play.py
+++ ml_play.py
@@ -25,6 +25,14 @@ def ml_loop():
 
     # 2. Inform the game process that ml process is ready before start the loop.
     comm.ml_ready()
+    ball_x =0
+    ball_y =0
+    m=0
+    delta_x=0
+    delta_y=0
+    predict_x=0
+
+
 
     # 3. Start an endless loop.
     while True:
@@ -43,10 +51,41 @@ def ml_loop():
             continue
 
         # 3.3. Put the code here to handle the scene information
+            
 
         # 3.4. Send the instruction for this frame to the game process
         if not ball_served:
             comm.send_instruction(scene_info.frame, PlatformAction.SERVE_TO_LEFT)
             ball_served = True
         else:
-            comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            last_ball_x = ball_x
+            last_ball_y = ball_y
+            ball_x = scene_info.ball[0]
+            ball_y = scene_info.ball[1]
+            delta_x = ball_x-last_ball_x # "+"means move right
+            delta_y = ball_y-last_ball_y # "+"means move downward
+            m=delta_y/delta_x
+        
+            platform_x = scene_info.platform[0]
+
+            if delta_y<0: #ball is moving upward
+                if platform_x<ball_x:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+            else: #ball is moving downward
+                predict_x=(400-ball_y+m*ball_x)/m
+
+                while predict_x>200 or predict_x<0:
+                    if predict_x>200:
+                        predict_x=400-predict_x
+                    if predict_x<0:
+                        predict_x=-predict_x
+
+                if predict_x-20 > platform_x:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_RIGHT)
+                else:
+                    comm.send_instruction(scene_info.frame, PlatformAction.MOVE_LEFT)
+
+                #y-y0=m(x-x0) (y-y0+m*x0)/m=x
+        
